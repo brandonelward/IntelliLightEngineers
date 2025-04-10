6d460de85dd80f8f937fb391036c59c68e97111a
s4205066 <114748904+s4205066@users.noreply.github.com>
2025-04-10 12:06:23 +0100

    simulationsetup page

--- pages/Simulate.py
+++ pages/Simulate.py
@@ -8,7 +8,7 @@ fp = os.path.dirname(__file__)
 sumo_upload_entry = st.file_uploader(label="Upload Sumo Files (.sumocfg, .xml)")
 if sumo_upload_entry is not None:
 
-    save_path = os.path.join(os.path.join(fp, '../simdata'), sumo_upload_entry.name)
+    save_path = os.path.join(os.path.join(fp, '../generated'), sumo_upload_entry.name)
     with open(save_path, "wb") as f:
         f.write(sumo_upload_entry.getbuffer())
     
@@ -16,7 +16,7 @@ if sumo_upload_entry is not None:
     st.write(f"File saved at: {save_path}")
 
 
-available_configs = os.listdir(os.path.join(fp, '../simdata'))
+available_configs = os.listdir(os.path.join(fp, '../generated'))
 available_configs = [sumocnfg for sumocnfg in available_configs if sumocnfg.endswith(".sumocfg")]
 sumo_config_entry = st.selectbox(label="Sumo Config Filename (.sumocfg)", options=available_configs)
 
@@ -24,7 +24,7 @@ sumo_config_entry = st.selectbox(label="Sumo Config Filename (.sumocfg)", option
 
 sim_duration_entry = st.number_input(label="Duration of simulation (timesteps)", min_value=10, max_value=99999, step=10)
 #sim_period_entry = st.number_input(label="Time Periods", min_value=2, max_value=50, step=1)
-output_filename_entry = st.text_input(label="Output data file (.csv)")
+output_filename_entry = st.text_input(label="Output data file name")
 gui_toggle = st.checkbox("Simulation GUI")
 
 submit_button = st.button("Simulate!")
--- pages/SimulationSetup.py
+++ pages/SimulationSetup.py
@@ -1,4 +1,47 @@
 import streamlit as st
+import os
+import xml.etree.cElementTree as ET
+
+fp = os.path.dirname(__file__)
+
+available_maps = os.listdir(os.path.join(fp, '../maps'))
+available_maps = [sumocnfg for sumocnfg in available_maps if sumocnfg.endswith(".net.xml")]
+map_select = st.selectbox(label="Simulation Map", options=available_maps)
+
+sim_duration_entry = st.number_input(label="Duration of simulation (timesteps)", min_value=10, max_value=99999, step=10)
+
+def WriteSimulationFile(simName, networkFile, duration):
+    routes = GenerateRoutesFile(networkFile, duration, simName)
+
+    config = ET.Element("configuration")
+    
+    input = ET.SubElement(config, "input")
+    time = ET.SubElement(config, "time")
+    report = ET.SubElement(config, "report")
+
+    ET.SubElement(input, 'net-file value="' + networkFile + '"')
+    ET.SubElement(input, 'route-files value="' + routes + '"')
+
+    ET.SubElement(time, 'begin value="0"')
+    ET.SubElement(time, 'end value="'+duration+'"')
+
+    ET.SubElement(report, 'collision-output value="collisions.xml"')
+
+    tree = ET.ElementTree(config)
+    fileName = os.path.join(fp, simName+".sumocfg")
+    tree.write(fileName)
+
+    st.write("Simulation file generated. " + fileName)
 
 
-#Integrate sbility to generate .sumocfg files
+def GenerateRoutesFile(networkFile, duration, outName):
+    routesPath = os.path.join(fp, '\\generated\\'+ outName+".rou.xml")
+    os.system("C:/Program Files (x86)/Eclipse/Sumo/tools/randomTrips.py -n "+ str(networkFile) +" -e "+ str(duration)+" -o "+str(routesPath))
+    st.write("Routes file Generated! " + routesPath)
+    return routesPath
+
+generateButton = st.button("Generate File")
+
+if generateButton:
+    WriteSimulationFile(simName=map_select.strip(".net.xml"), networkFile=map_select, duration=sim_duration_entry)
+
